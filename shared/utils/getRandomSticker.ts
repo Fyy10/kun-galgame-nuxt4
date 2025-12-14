a9b111f65feb14e2f932a4649643cf84474a6b85
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-12-14 22:26:18 +0800

    fix(utils): Error: [unimport] failed to find "useState" imported from "#imports"

--- shared/utils/getRandomSticker.ts
+++ shared/utils/getRandomSticker.ts
@@ -1,15 +1,17 @@
-import { useState } from '#imports'
 import { randomNum } from './random'
 import { kungal } from '../../app/config/kungal'
+
+const stickerCache: Record<string, string> = {}
 
 export const getRandomSticker = (id: string) => {
   const key = `random-sticker-${id}`
 
-  const stickerUrl = useState<string>(key, () => {
+  if (!stickerCache[key]) {
     const randomPackIndex = randomNum(1, 5)
     const randomStickerIndex = randomNum(1, 80)
-    return `${kungal.domain.sticker}/stickers/KUNgal${randomPackIndex}/${randomStickerIndex}.webp`
-  })
+    stickerCache[key] =
+      `${kungal.domain.sticker}/stickers/KUNgal${randomPackIndex}/${randomStickerIndex}.webp`
+  }
 
-  return stickerUrl
+  return stickerCache[key]
 }
